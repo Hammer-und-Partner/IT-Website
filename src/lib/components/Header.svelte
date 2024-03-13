0540bd91bdd0c13648a0a23982ddb3e3e4970e21
Julian Hammer <jh@moinjulian.com>
2024-03-13 10:21:00 +0000

    Update styles and add header component

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,13 @@
+<header>
+	<img src="/logo.png" alt="Hammer & Partner Logo" />
+
+	<h1>Hammer partners IT GmbH</h1>
+	<small>IT Diesntleistungen</small>
+</header>
+
+<style lang="scss">
+	header {
+		background-color: var(--header-color);
+		color: var(--header-font-color);
+	}
+</style>

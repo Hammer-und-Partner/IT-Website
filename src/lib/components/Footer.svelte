bded82b5bc441a0b1fdb0fa1db2a9be834a6a2bb
Julian Hammer <jh@moinjulian.com>
2024-03-13 18:33:14 +0000

    Add link styles

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,19 +1,19 @@
 <footer>
-    <div class="center">
-        <div class="adress">
-            <p>Außer der Schleifmühle 75</p>
-            <p>28203 Bremen</p>
-        </div>
-    
-        <div class="contact">
-            <p>
-                <a href="tel:49421369040">Telefon: +49 421 369 04-0</a>
-            </p>
-            <p>
-                <a href="tel:494213690420">Fax: +49 421 369 04-20</a>
-            </p>
-        </div>
-    </div>
+	<div class="center">
+		<div class="adress">
+			<p>Außer der Schleifmühle 75</p>
+			<p>28203 Bremen</p>
+		</div>
+
+		<div class="contact">
+			<p>
+				<a href="tel:49421369040">Telefon: +49 421 369 04-0</a>
+			</p>
+			<p>
+				<a href="tel:494213690420">Fax: +49 421 369 04-20</a>
+			</p>
+		</div>
+	</div>
 </footer>
 
 <style lang="scss">
@@ -21,18 +21,18 @@
 		background-color: var(--header-color);
 		color: var(--header-font-color);
 		box-shadow: 1px 1px 5px 2px var(--shadow-color);
-        height: calc(124px + 1rem);
+		height: calc(124px + 1rem);
 
 		display: flex;
 		flex-direction: row;
 
-        .center {
-            margin: 0 auto;
-            display: flex;
-            flex-direction: row;
-            justify-content: center;
-            align-items: center;
-            width: 100%;
-        }
+		.center {
+			margin: 0 auto;
+			display: flex;
+			flex-direction: row;
+			justify-content: center;
+			align-items: center;
+			width: 100%;
+		}
 	}
 </style>

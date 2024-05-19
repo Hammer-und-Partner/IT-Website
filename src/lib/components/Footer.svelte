0ace532ba51c4d07efa289b0c8608fa90d8fa4b0
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2024-05-19 12:49:45 +0100

    Refactor Footer component styles

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,11 +1,10 @@
 <footer>
 	<div class="center">
-		<div class="adress">
+		<div class="address">
 			<p>Außer der Schleifmühle 75</p>
 			<p>28203 Bremen</p>
 		</div>
 		<div class="border"></div>
-		<!-- Add a div with class "border" -->
 		<div class="contact">
 			<p>
 				Telefon: <a href="tel:49421369040"> +49 421 369 04-0</a>
@@ -23,7 +22,6 @@
 		color: var(--header-font-color);
 		box-shadow: 1px 1px 5px 2px var(--shadow-color);
 		height: calc(124px + 1rem);
-
 		display: flex;
 		flex-direction: row;
 
@@ -35,7 +33,8 @@
 			align-items: center;
 			width: 100%;
 
-			.adress {
+			.address,
+			.contact {
 				margin-left: 2rem;
 				display: flex;
 				flex-direction: column;
@@ -43,16 +42,29 @@
 			}
 
 			.border {
-				border-left: 1px solid var(--header-font-color); /* Add border style */
-				height: calc(1rem * 2); /* Set the height to match the parent div */
-				margin: 0 2rem; /* Adjust the margin as needed */
+				border-left: 1px solid var(--header-font-color); // Add border style
+				height: calc(1rem * 2); // Set the height to match the parent div
+				margin: 0 2rem; // Adjust the margin as needed
 			}
+		}
 
-			.contact {
-				margin-left: 2rem;
-				display: flex;
+		// Styles for phones
+		@media (max-width: 600px) {
+			.center {
 				flex-direction: column;
-				justify-content: center;
+
+				.address,
+				.contact {
+					margin-left: 0;
+				}
+
+				.border {
+					border-left: none;
+					border-bottom: 1px solid var(--header-font-color); // Change border to bottom
+					width: 80%; // Adjust width as needed
+					height: 0; // No height needed for horizontal border
+					margin: 1rem 0; // Adjust margin as needed
+				}
 			}
 		}
 	}
